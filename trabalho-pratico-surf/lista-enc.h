6bedffdccee0b9760adf66fb5cd69112f6972f33
Levil9490 <levil9490@gmail.com>
2025-12-20 17:54:42 -0300

    add: struct da lista

---
+++ trabalho-pratico-surf/lista-enc.h
@@ -0,0 +1,10 @@
+typedef struct NO_DA_LISTA_ENC{
+    int codigo;
+    struct NO *prox;
+}NO_DA_LISTA_ENC;
+
+typedef struct LISTA_ENC_PRODUT{
+    struct NO *inicio;
+    struct NO *fim;
+    int tam;
+}LISTA_ENC_PRODUT;
