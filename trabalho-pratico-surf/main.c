c23275aea17d0fea6e4a2d57ed7c94ff4465f722
Levil9490 <levil9490@gmail.com>
2026-01-17 10:25:44 -0300

    fix: lista-dup-enc renomeado para produtos

--- trabalho-pratico-surf/main.c
+++ trabalho-pratico-surf/main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "lista-dup-enc.h"
-#include "lista-enc.h"
+#include "produtos/produtos.h"
+#include "lista-enc/lista-enc.h"
 
 int main(){
-/*    adicionar_produto(10, 20.45, "Quilha", "...");
+    adicionar_produto(10, 20.45, "Quilha", "...");
     adicionar_produto(20, 22.90, "Leash", "...");
     adicionar_produto(30, 10.25, "Outro Leash", "...");
     adicionar_produto(40, 100.99, "Prancha", "Nao sei quanto custa uma");
@@ -12,7 +12,7 @@ int main(){
     remover_produto(20);
     remover_produto(50);
     imprimir_produtos();
-*/
+
     LISTA_ENC_PRODUT *lista = construir_lista();
     add_na_lista_enc(lista, 1234);
 
---
+++ trabalho-pratico-surf/produtos/produtos.h
@@ -0,0 +1,21 @@
+#ifndef PRODUTOS_H
+#define PRODUTOS_H
+
+typedef struct PRODUTO{
+    int codigo;
+    float preco;
+    char *tipo;
+    char *descricao;
+    struct PRODUTO *prox;
+    struct PRODUTO *ant;
+}PRODUTO;
+
+extern PRODUTO *inicio;
+extern PRODUTO *fim;
+extern int tam;
+
+void adicionar_produto(int codigo, float preco, char *tipo, char *descricao);
+void imprimir_produtos();
+void remover_produto(int codigo);
+
+#endif
